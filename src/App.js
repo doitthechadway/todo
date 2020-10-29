08985896888e5cfca863ae2fcbc74ca081cc7a3c
doitthechadway <waychad1@gmail.com>
2020-10-29 16:03:47 -0500

    Completed 1+2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import {ToDoBanner} from './ToDoBanner';
+import 'bootstrap/dist/css/bootstrap.css'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  //  Above we have created a class called App the extends the functionality of the Component class
+
+  //  The export keyword above makes the class available for use outside of the JS file where it is created
+  constructor() {
+
+    super();
+
+    // React components have a special property called "state". The "state" is used to define the state of data (props)
+
+    this.state = {
+      userName: "Validata",
+      todoItems: [
+        { action: "Move", done: false },
+        { action: "Sap a sentry", done: false },
+        { action: "Go invisible", done: false },
+        { action: "Run Around in Circles", done: false },
+        { action: "Buy a Hilux", done: true }
+      ]
+    }
+  } //END Constructor
+
+  render = () =>
+    <div id="todoBanner">
+      {/*Features 1 & 2 */}
+
+      <ToDoBanner
+        userName = {this.state.userName}
+        todoItems = {this.state.todoItems}
+
+      />
     </div>
-  );
+
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------------------------- DEFAULT SCAFFOLDED CODE ----------------------------
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/ToDoBanner.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+// FEATURES 1 AND 2
+export class ToDoBanner extends Component {
+  render = () =>
+  <h4 className="bg-primary text-white text-center p-2">
+      {this.props.userName}'s To-Do List:
+      <p>| {this.props.todoItems.filter(x => !x.done).length} unfinished items |</p>
+      
+  </h4>
+};
