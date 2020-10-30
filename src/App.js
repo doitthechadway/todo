74cc1df98a1835ec2099c4806c3c2a9aafeb0788
doitthechadway <waychad1@gmail.com>
2020-10-30 10:25:09 -0500

    5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import React, { Component } from 'react';
-import {ToDoBanner} from './ToDoBanner';
-import 'bootstrap/dist/css/bootstrap.css'; 
+import { ToDoBanner } from './ToDoBanner';
+import { ToDoRow } from './ToDoRow';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default class App extends Component {
   //  Above we have created a class called App the extends the functionality of the Component class
 
-  //  The export keyword above makes the class available for use outside of the JS file where it is created
+  //  The export keyword above makes the class available for use outside of the JS file where 
+  //it is created
   constructor() {
 
     super();
 
-    // React components have a special property called "state". The "state" is used to define the state of data (props)
+    // React components have a special property called "state". The "state" is used to define
+    // the state of data (props)
 
     this.state = {
       userName: "Validata",
@@ -25,15 +28,52 @@ export default class App extends Component {
     }
   } //END Constructor
 
+  // -------------- Function to display table rows ----------------
+  todoTableRows = (isTaskComplete) => this.state.todoItems.filter(
+    x => x.done == isTaskComplete).map(y => <ToDoRow
+      key = {y.action}
+      myToDoItem = {y}
+      callback = {this.toggleToDo}  //The callback will be invoked (executed, run) when everything in <ToDoRow> is finished AND the user clicks the input box
+      // The data passed into the callback from the ToDoRow component is passed automatically into the function defined in the callback
+
+      />
+);
+
+
+// Feature 4
+//toggleToDo
+//Function to toggle "done" property to true/false (opposite of what it was)
+  //  .setState allows the in memory data to be updated
+  //  When setState is invoked, React will make a new object with the changes.  Under the hood React will compare the new object with the DOM version of the object.  If there is a difference between those 2 objects then the DOM will get re-drawn (NOT a reload) and then we see the changes.
+
+toggleToDo = (checkedToDoItem) => this.setState(
+  {
+    todoItems: this.state.todoItems.map(
+      x => x.action == checkedToDoItem.action ? { ...x, done: !x.done} : x
+    )
+  }
+);
+
+
+
   render = () =>
     <div id="todoBanner">
       {/*Features 1 & 2 */}
 
       <ToDoBanner
-        userName = {this.state.userName}
-        todoItems = {this.state.todoItems}
-
+        userName={this.state.userName}
+        todoItems={this.state.todoItems}
       />
+      {/*Features 3 & 4*/}
+      <table className="table table-striped table-bordered">
+        <thead>
+          <th>Description</th>
+          <th>Mark Complete</th>
+        </thead>
+        <tbody>
+          {this.todoTableRows(false)}
+        </tbody>
+      </table>
     </div>
 
 }
---
+++ src/ToDoRow.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+// FEATURES 3 AND 4
+export class ToDoRow extends Component {
+  render = () =>
+  <tr>
+      <td>
+            {this.props.myToDoItem.action}
+      </td>
+      <td>
+            <input type="checkbox"
+                  checked={this.props.myToDoItem.done}
+                  onChange={() => this.props.callback(this.props.myToDoItem)} />
+      </td>
+  </tr>
+}
